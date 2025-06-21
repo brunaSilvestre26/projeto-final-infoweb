a7b5ac03c0708907bf99a4279dd28968bffd2efb
Bruna Silvestre <silvestre.bruna8@outlook.pt>
2025-06-21 15:27:37 +0100

    Added filters to homepage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,10 @@
 import { ArticleList } from '@/components/ArticleList'
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
 import { useGetApprovedArticlesQuery } from '@/hooks/articles'
+import { useGetSourcesQuery } from '@/hooks/sources'
 import { createFileRoute } from '@tanstack/react-router'
+import { Filter } from 'lucide-react'
+import { useState } from 'react'
 
 export const Route = createFileRoute('/')({
   component: Index,
@@ -10,13 +14,54 @@ function Index() {
   // TODO: Implement search functionality
   // TODO: Implement pagination
 
-  const articles = useGetApprovedArticlesQuery()
+  const [showFilters, setShowFilters] = useState(false)
+  const [selectedSource, setSelectedSource] = useState<string | undefined>('all')
 
-  if (articles.isLoading) {
+  const articles = useGetApprovedArticlesQuery()
+  console.log('🚀 ~ Index ~ articles:', articles.data)
+  const sources = useGetSourcesQuery()
+
+  const filteredArticles =
+    selectedSource === 'JornalismoUBI'
+      ? articles.data?.filter((article) => article.source_id === null)
+      : selectedSource === 'all'
+        ? articles.data
+        : articles.data?.filter((article) => article.source_id === selectedSource)
+
+  if (articles.isLoading || sources.isLoading) {
     return <div>A carregar...</div>
-  } else if (articles.isError) {
-    return <div>Erro ao carregar artigos</div>
-  } else if (articles.data) {
-    return <ArticleList articles={articles.data} />
   }
+
+  if (articles.isError || !articles.data) {
+    return <div>Erro ao carregar artigos</div>
+  }
+
+  return (
+    <div className="flex flex-col items-center w-full">
+      <div className="flex flex-col gap-2 w-full justify-center items-end mb-4 pr-10">
+        <button
+          onClick={() => setShowFilters(!showFilters)}
+          className="gap-1 flex items-center text-muted-foreground hover:text-gray-900 transition-colors cursor-pointer"
+        >
+          <Filter className="h-4 w-4" />
+          <span>Filtros</span>
+        </button>
+        {showFilters && (
+          <Select value={selectedSource} onValueChange={setSelectedSource}>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Fonte" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value="all">Todas as fontes</SelectItem>
+                <SelectItem value="JornalismoUBI">JornalismoUBI</SelectItem>
+                {sources.data?.map((source) => <SelectItem value={source.id}>{source.name}</SelectItem>)}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        )}
+      </div>
+      <ArticleList articles={filteredArticles} />
+    </div>
+  )
 }
