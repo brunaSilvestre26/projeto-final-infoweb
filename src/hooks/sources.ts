00742310ea29bf03a17a852c9d0c25138288ffdd
Bruna Silvestre <silvestre.bruna8@outlook.pt>
2025-06-21 12:38:24 +0100

    Added Source related hook

---
+++ src/hooks/sources.ts
@@ -0,0 +1,16 @@
+import { supabase } from '@/supabase/supabase'
+import { useQuery } from '@tanstack/react-query'
+
+// hooks
+export const useGetSourcesQuery = () => {
+  return useQuery({
+    queryKey: ['sources'],
+    queryFn: getSources,
+  })
+}
+
+// funções
+export const getSources = async () => {
+  const { data } = await supabase.from('source').select('*')
+  return data
+}
